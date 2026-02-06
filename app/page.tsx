50b7d879c29f23a669886ca356e12a9f5d2c305b
shahoriarratul <shahoriar.ratul@gmail.com>
2026-02-06 23:28:55 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-2xl font-bold">My Next.js App</h1>
-      <nav>
-        <ul className="flex space-x-4">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-      </nav>
+    <div>
+      
     </div>
   );
 }
