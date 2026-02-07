f306592309a0655136aaf062773c10e38bd64032
shahoriarratul <shahoriar.ratul@gmail.com>
2026-02-07 02:45:26 +0100

    Navbar done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import Navbar from "./sections/Navbar";
+
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className="relative w-screen min-h-screen overflow-x-auto">
+    <Navbar />
+    <section id="home" className='min-h-screen bg-amber-950'/>
+    <section id="service" className='min-h-screen'/>
     </div>
   );
 }
